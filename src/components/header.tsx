4ed18e8110c8156d3521a3c05cd725a429f484b3
HamedTaherpour <dev.hamedt@gmail.com>
2024-11-26 13:42:18 +0330

    fixed responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {usePathname} from 'next/navigation';
 import Menu from '@/assets/icon/menu.svg';
+import MenuOpen from '@/assets/icon/menu-open.svg';
+import X from '@/assets/icon/x.svg';
+import {useState} from 'react';
 
 const navbarLinks = [
   {name: 'Home', url: '/'},
@@ -18,6 +21,7 @@ const navbarLinks = [
 
 const Header = () => {
   const pathname = usePathname();
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <header className="w-full bg-white">
@@ -37,10 +41,31 @@ const Header = () => {
           </ul>
         </div>
         <div>
-          <button className="block lg:hidden">
-            <Menu />
+          <button onClick={() => setOpenMenu(!openMenu)} className="block lg:hidden">
+            {!openMenu ? <Menu /> : <MenuOpen />}
           </button>
         </div>
+        {openMenu && (
+          <div className="fixed inset-0 z-50 h-screen w-full bg-black/50 p-6 backdrop-blur">
+            <div className="h-16"></div>
+            <div className="w-full rounded-lg bg-white/90 px-6 py-10 shadow-2xl">
+              <div className="flex flex-row items-center justify-end">
+                <button onClick={() => setOpenMenu(false)}>
+                  <X className="size-6 stroke-black" />
+                </button>
+              </div>
+              <ul className="mt-6 flex flex-col items-center divide-y divide-red-500">
+                {navbarLinks.map((item) => (
+                  <li key={item.url} className="flex h-16 w-full items-center">
+                    <Link href={item.url} className={`app-base-transform w-full text-left text-xl underline-offset-4 hover:underline ${pathname === item.url && 'text-primary decoration-primary'}`}>
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
